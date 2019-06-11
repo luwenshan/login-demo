2a32b0e061551d581ddbf5d40b465064f915f440
luwenshan <lws1076694749@126.com>
2019-06-11 13:40:47 +0800

    add list style

--- src/views/List.vue
+++ src/views/List.vue
@@ -1,7 +1,16 @@
 <template>
   <div>
     <ul>
-      <li v-for="(item, index) in pageList" :key="index">{{item.title}}-{{item.content}}</li>
+      <li
+        class="item"
+        v-for="(item, index) in pageList"
+        :key="index"
+        v-on:click="select(index)"
+        v-bind:class="{ selected: index == selectIndex}"
+      >
+        <p class="title">{{item.title}}</p>
+        <p class="content">{{item.content}}</p>
+      </li>
     </ul>
   </div>
 </template>
@@ -11,6 +20,20 @@ import store from "@/store";
 export default {
   name: "List",
   store,
+  data() {
+    return {
+      selectIndex: -1
+    };
+  },
+  methods: {
+    select(index) {
+      if (this.selectIndex == index) {
+        this.selectIndex = -1;
+      } else {
+        this.selectIndex = index;
+      }
+    }
+  },
   computed: {
     pageList() {
       return store.state.list;
@@ -18,4 +41,27 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.item {
+  display: block;
+  width: 94%;
+  margin: 20px auto;
+  padding: 15px;
+  background-color: lightgrey;
+  border-radius: 3px;
+}
+.title {
+  font-size: 18px;
+  font-weight: bold;
+}
+.content {
+  margin-top: 10px;
+}
+.selected{
+  background-color: green;
+  color: white;
+}
+</style>
+
 
